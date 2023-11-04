cc9978705566c501a9dc56427d62b44cd66bb09b
Code Sandbox <hello@codesandbox.io>
2023-11-04 03:41:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c84f4511c520733ed676fb3098f03300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad273e78dc0cb313731349018f6acaaf",
+    "url": "https://hackdit.github.io/csb-ny5l46/index.html"
+  },
+  {
+    "revision": "0a6725ba7914cc42a4cb",
+    "url": "https://hackdit.github.io/csb-ny5l46/static/css/main.0427df99.chunk.css"
+  },
+  {
+    "revision": "594b2a2c5c580dd82a20",
+    "url": "https://hackdit.github.io/csb-ny5l46/static/js/2.6d0bdfff.chunk.js"
+  },
+  {
+    "revision": "0a6725ba7914cc42a4cb",
+    "url": "https://hackdit.github.io/csb-ny5l46/static/js/main.007196ef.chunk.js"
+  },
+  {
+    "revision": "0efe049a0fb6a2bc177a",
+    "url": "https://hackdit.github.io/csb-ny5l46/static/js/runtime-main.189c5090.js"
+  }
+]);
